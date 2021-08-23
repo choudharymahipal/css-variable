e32dcbd8dbf5b3a00c0b92082346fdb194528c2a
choudharymahipal <mschoudhary81@gmail.com>
2021-08-24 00:10:51 +0530

    on click to change color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, ElementRef, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  constructor(private el: ElementRef) {}
+
+  onChangeColor(color) {
+    (this.el.nativeElement as HTMLElement).style.setProperty(
+      '--mahi-color',
+      color
+    );
+  }
 }
